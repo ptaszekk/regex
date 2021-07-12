853337a43fcd66a6f33c6df954ca2c0a27f63185
jakub.ptaszek <jakub.ptaszek@infor.com>
2021-07-12 15:24:09 +0200

    keywordRegex works

--- regex.js
+++ regex.js
@@ -5,17 +5,27 @@ let regexFunc = /function?\s+\(\s+([a-zA-Z]*)\s+\)\s+/g;
 let regex = /function\s+[a-zA-Z]*/g;
 let regexArrow = /|\(\)\(=>\)\s+?/
 
+
+const regexKeyword = (function() {
+    const keyWords = ["await", "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "enum", "export", "extends", "false", "finally", "for", "function", "if", "implements", "import", "in", "instanceof", "interface", "let", "new", "null", "package", "private", "protected", "public", "return", "static", "super", "switch", "this", "throw", "true", "try", "typeof", "var", "void", "while", "with", "yield"]
+    let regex = ''
+    keyWords.forEach(word => regex = `${regex + word}|`)
+    return `/${regex}/g`;
+})()
+console.log(regexKeyword)
 let str = 'function afsaf function fajhHDHJDdss function FSJKFJfkjs      ';
 
-function findFunction(str){
-    console.log( str.match(regex))
+function findFunction(str) {
+    console.log(str.match(regex))
 }
 
-function count (str) {
+function count(str) {
     console.log(str.match(regex).length)
     return str.match(regex).length
 }
+
 count(str)
 findFunction(str);
 
-function whatever (){}
+function whatever() {
+}
